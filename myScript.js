6ba287fac4980a905efbf70fc49bcd4418c309e6
woratat <84006033+woratat@users.noreply.github.com>
2021-06-11 11:42:09 +0700

    2.Add notification

--- myScript.js
+++ myScript.js
@@ -7,30 +7,30 @@ var counter=0
 function onOKClicked(e){
     e.stopPropagation();
     // alert('ok clicked')
-    text = text+"ok\n"
-    button.innerText=text //innerText ใช้ \n
-    // addNode()
+    // text = text+"ok\n"
+    // button.innerText=text //innerText ใช้ \n
+    addNode()
 }
 function onCancelClicked(e){
     e.stopPropagation();
     // alert('cancel clicked')
-    text = text+"cancel\n"
-    button.innerText=text
+    // text = text+"cancel\n"
+    // button.innerText=text
 }
 function onNoClicked(e){
     e.stopPropagation();
     // alert('no clicked')
-    text = text+"no\n"
-    button.innerText=text
+    // text = text+"no\n"
+    // button.innerText=text
 }
 function onConClicked(e){
     // alert('container clicked')
-    text = ""
-    button.innerText = ""
+    // text = ""
+    // button.innerText = ""
 }
 function addNode(){
     newNode = document.createElement('div') //สร้าง div ใน output-container
-    newNode.classList.add('flex-item') //เพิ่ม flex-item ใน classList เช่น <div class="flex-item" id="0">1</div>
+    newNode.classList.add('flex-item') //เพิ่ม flex-item ใน div เช่น <div class="flex-item" id="0">1</div>
     newNode.setAttribute('id',counter) // set ค่า id ด้วย counter นับจาก 0 เพิ่มไปเรื่อยๆเมื่อกดปุ่ม <div class="flex-item" id="0">1</div>
     newNode.innerText = counter  //แสดงข้อความจาก counter ลงใน newNode หรือ div
     counter = counter+1  
---
+++ temScript.js
@@ -0,0 +1,25 @@
+var searchButton = document.getElementById('searchButton')
+var inputText = document.getElementById('inputText')
+var output = document.getElementById('output')
+
+var noti = document.getElementById('searchButton')
+var notiOut = document.getElementById('notiOut')
+var counter=0
+
+function addText(){
+    let text = inputText.value  //value ค่าที่เก็บไว้ใน text field
+    console.log(text)
+    let newButton = document.createElement('button')
+    newButton.classList.add('btn')
+    newButton.classList.add('btn-outline-primary')
+    newButton.classList.add('m-2')
+    newButton.setAttribute('type','button')
+    newButton.innerText = text
+    output.appendChild(newButton)
+}
+function addNum(){
+    notiOut.innerText = counter
+    counter = counter+1;
+    notiOut.innerText = counter
+}
+searchButton.addEventListener('click',addText)
